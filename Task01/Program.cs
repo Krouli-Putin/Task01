4f470a4a243845f12130e0392546e341568888d3
John <jkrouli@list.ru>
2023-05-03 15:49:24 +0300

    Task01

---
+++ Task01/Program.cs
@@ -0,0 +1,9 @@
+﻿int number = new Random().Next(10, 100);
+Console.WriteLine($"Cлучайное число из отрезка 10 - 100 => {number}");
+
+int festDigit = number / 10;
+int secondDigit = number % 10;
+
+if (festDigit > secondDigit)
+Console.WriteLine($"Наибольша цифра числа -> {festDigit}");
+else Console.WriteLine($"Наибольшая цифра числа -> {secondDigit}");

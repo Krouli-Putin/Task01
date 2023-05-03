12ea4b0a45e20a298de2f31009c58e0f452b4d65
John <jkrouli@list.ru>
2023-05-03 16:34:03 +0300

    Task02

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,9 +1,20 @@
 ﻿int number = new Random().Next(10, 100);
 Console.WriteLine($"Cлучайное число из отрезка 10 - 100 => {number}");
 
-int festDigit = number / 10;
-int secondDigit = number % 10;
+//int festDigit = number / 10;
+//int secondDigit = number % 10;
 
-if (festDigit > secondDigit)
-Console.WriteLine($"Наибольша цифра числа -> {festDigit}");
-else Console.WriteLine($"Наибольшая цифра числа -> {secondDigit}");
+//if (festDigit > secondDigit)
+//    Console.WriteLine($"Наибольша цифра числа -> {festDigit}");
+//else Console.WriteLine($"Наибольшая цифра числа -> {secondDigit}");
+
+
+int maxDigit = MaxDigit(number);
+Console.WriteLine($"Наибольшая цифра числа -> {maxDigit}");
+int MaxDigit(int num)
+{
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    if (firstDigit > secondDigit) return firstDigit;
+    else return secondDigit;
+}
